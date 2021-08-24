77fa577b9a8eab9de607d50877bb2a1960a68b79
vishaksathishkumar <74096175+vishaksathishkumar@users.noreply.github.com>
2021-08-24 19:05:36 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@ const Constraint = Matter.Constraint ;
 const Bodies = Matter.Bodies;
 var myengine
 var myworld
+var block=[]
 
 
 function setup() {
@@ -12,12 +13,17 @@ function setup() {
   myworld=myengine.world;
   ball=new Ball(200,200,25);
   rope= new Rope(ball.body,{x:200,y:50})
+  for (var j = 0; j <=width-10; j=j+50) 
+  {
+    block.push(new Box(20,60,50,50));
+  }
 }
 
 function draw() {
   background(0);
   ball.display();
   rope.display();
+  block.display();
 
 }
 
